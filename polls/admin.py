3c2b74f1a47c0eb9d55f5efe1d5b9443d025638d
carmine153 <carminepignatiello2005@gmail.com>
2026-07-10 11:38:53 +0200

    Add files via upload

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Poll, Choice
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.admin import TokenAdmin
 
 admin.site.register(Poll)
 admin.site.register(Choice)
+admin.site.register(Token, TokenAdmin)
 
 # Register your models here.
